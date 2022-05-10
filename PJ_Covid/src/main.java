7af8bc8ee593d902a4d79839df77bfa330411b7c
Minh Duong <quangminh29102001@gmail.com>
2022-05-10 21:36:45 +0700

    Create main.java

---
+++ PJ_Covid/src/main.java
@@ -0,0 +1,14 @@
+import QUANLY.BENHNHAN;
+import QUANLY.USERS;
+import Views.FirstPage;
+import Views.HomePage;
+
+import java.util.ArrayList;
+
+public class main {
+
+    public static void main(String[] args) {
+        FirstPage firstPage = new FirstPage();
+        firstPage.showFirstPage();
+    }
+}
